fdf31a086a9e9bd6335cc656005628ea1d161b6e
=argon <yunguta@gmail.com>
2016-12-15 01:05:21 +0000

    static files

--- ait_ilcdmp/urls.py
+++ ait_ilcdmp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     # Examples:
     # url(r'^$', 'ait_ilcdmp.views.home', name='home'),
@@ -10,3 +11,5 @@ urlpatterns = [
     url(r'^student/', include('info_system.urls.student', namespace='student')),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
